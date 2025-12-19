876150e276e8cd4ba8eca8baa160b59bb52b4254
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-19 21:19:46 +0700

    setup ContainerApp untuk dependency injection dan konfigurasi Retrofit client

---
+++ app/src/main/java/com/example/questapi_050/repositori/ContainerApp.kt
@@ -0,0 +1,36 @@
+package com.example.questapi_050.repositori
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+
+interface ContainerApp {
+    val repositoryDataSiswa : RepositoryDataSiswa
+}
+
+class DefaultContainerApp : ContainerApp {
+    private val baseurl = "http://10.0.2.2/umyTI"
+
+    val logging = HttpLoggingInterceptor().apply {
+        level = HttpLoggingInterceptor.Level.BODY
+    }
+    val klien = OkHttpClient.Builder()
+        .addInterceptor(interceptor = logging)
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .baseUrl(baseurl)
+        .addConverterFactory(
+            Json{
+                ignoreUnknownKeys =true
+                prettyPrint = true
+                isLenient = true
+            }.asConverterFactory(contentType = "application/json".toMediaType())
+        )
+        .client(klien)
+        .build()")
+        )
+}
