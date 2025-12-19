e49d6d2a00a196290fbb36a5e413f8b892ccadaf
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-19 21:30:55 +0700

    implementasi interface RepositoryDataSiswa dan class JaringanRepositoryDataSiswa untuk manajemen data API

---
+++ app/src/main/java/com/example/questapi_050/repositori/RepositoryDataSiswa.kt
@@ -0,0 +1,17 @@
+package com.example.questapi_050.repositori
+
+import com.example.questapi_050.apiservice.ServiceApiSiswa
+import com.example.questapi_050.modeldata.DataSiswa
+import retrofit2.Response
+
+interface RepositoryDataSiswa {
+    suspend fun getDataSiswa() : List<DataSiswa>
+    suspend fun postDataSiswa(dataSiswa: DataSiswa) :retrofit2.Response<Void>
+}
+
+class JaringanRepositoryDataSiswa(
+    private val serviceApiSiswa: ServiceApiSiswa
+): RepositoryDataSiswa{
+    override suspend fun getDataSiswa(): List<DataSiswa> = serviceApiSiswa.getSiswa()
+    override suspend fun postDataSiswa(dataSiswa: DataSiswa): retrofit2.Response<Void> = serviceApiSiswa.postSiswa(dataSiswa)
+}
