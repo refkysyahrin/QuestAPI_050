c5a4b199ca911f94d433da3b1b895d6ed0e9c059
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-19 21:35:01 +0700

    membuat object DestinasiHome untuk definisi rute dan judul halaman utama

---
+++ app/src/main/java/com/example/questapi_050/uicontroller/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.questapi_050.uicontroller.route
+
+import com.example.questapi_050.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
