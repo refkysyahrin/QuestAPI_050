e27f9102a592b5d0d8db0130447013f2af4c9343
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 15:16:03 +0700

    implementasi UI DetailSiswaScreen dengan scaffold, navigasi edit, dan logika hapus data

--- app/src/main/java/com/example/questapi_050/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_050/view/HalamanDetail.kt
@@ -1,11 +1,27 @@
 package com.example.questapi_050.view
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_050.R
 import com.example.questapi_050.viewmodel.DetailViewModel
+import com.example.questapi_050.viewmodel.StatusUIDetail
 import com.example.questapi_050.viewmodel.provider.PenyediaViewModel
+import kotlinx.coroutines.launch
+import retrofit2.http.Body
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,5 +31,42 @@ fun DetailSiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack
+            )
+        },
+        floatingActionButton = {
+            val uiState = viewModel.statusUIDetail
+            FloatingActionButton(
+                onClick = {
+                    when(uiState){ is StatusUIDetail.Success->
+                        navigateToEditItem(uiState.satusiswa.id) else->{}}
+                },
+                shape = MaterialTheme.shapes.medium,
+                modifier = Modifier.padding(dimensionResource(id = R.dimen.padding_large))
 
+            ){
+                Icon(
+                    imageVector = Icons.Default.Edit,
+                    contentDescription = stringResource(R.string.update),
+                )
+            }
+        }, modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        BodyDetailDataSiswa(
+            statusUIDetail = viewModel.statusUIDetail,
+            onDelete = { coroutineScope.launch {
+                viewModel.hapusSatuSiswa()
+                navigateBack()
+            }},
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+        )
+    }
 }
