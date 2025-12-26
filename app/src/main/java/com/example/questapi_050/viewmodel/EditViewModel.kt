fdbe8c360cb392149052311341e087029a19ab25
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 14:18:14 +0700

    inisialisasi EditViewModel dengan savedStateHandle untuk menangani argumen navigasi

--- app/src/main/java/com/example/questapi_050/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_050/viewmodel/EditViewModel.kt
@@ -1,2 +1,14 @@
 package com.example.questapi_050.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import com.example.questapi_050.modeldata.UIStateSiswa
+import com.example.questapi_050.repositori.RepositoryDataSiswa
+
+class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
+    var uiStateSiswa by mutableStateOf(UIStateSiswa())
+        private set
+    }
