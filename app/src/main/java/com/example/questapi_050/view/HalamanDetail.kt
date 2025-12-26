2ea42d9695261ea0905628ab8704b20072b1ab07
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 15:43:02 +0700

    membuat komponen reusable BarisDetailData untuk menampilkan label dan value dalam layout row

--- app/src/main/java/com/example/questapi_050/view/HalamanDetail.kt
+++ app/src/main/java/com/example/questapi_050/view/HalamanDetail.kt
@@ -1,7 +1,10 @@
 package com.example.questapi_050.view
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -26,6 +29,8 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questapi_050.R
 import com.example.questapi_050.modeldata.DataSiswa
@@ -163,5 +168,15 @@ fun DetailDataSiswa(
                 )
             )
         }
+    }
+}
+@Composable
+private fun BarisDetailData(
+    @StringRes labelResID: Int, itemDetail: String, modifier: Modifier = Modifier
+){
+    Row(modifier = modifier) {
+        Text(text = stringResource(labelResID))
+        Spacer(modifier = Modifier.weight(1f))
+        Text(text = itemDetail, fontWeight = FontWeight.Bold)
     }
 }
