d5607b45413ddf1df793fb70f80ec738c07551f4
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 16:24:17 +0700

    implementasi EditSiswaScreen menggunakan EntrySiswaBody dan integrasi logika update via EditViewModel

--- app/src/main/java/com/example/questapi_050/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_050/view/HalamanEdit.kt
@@ -1,11 +1,17 @@
 package com.example.questapi_050.view
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.questapi_050.viewmodel.EditViewModel
 import com.example.questapi_050.viewmodel.provider.PenyediaViewModel
-import java.lang.reflect.Modifier
+import kotlinx.coroutines.launch
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -15,5 +21,27 @@ fun EditSiswaScreen(
     modifier: Modifier = Modifier,
     viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
-
+    Scaffold(
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = onNavigateUp
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        val coroutineScope = rememberCoroutineScope()
+        EntrySiswaBody(
+            uiStateSiswa = viewModel.uiStateSiswa,
+            onSiswaValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.editSatuSiswa()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier.padding(innerPadding)
+        )
+    }
 }
