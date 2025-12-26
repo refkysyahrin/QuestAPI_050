35526aa32ada7d6be621ca1648c7e888bca65b3d
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 16:17:12 +0700

    inisialisasi EditSiswaScreen dengan injeksi EditViewModel dan parameter navigasi

--- app/src/main/java/com/example/questapi_050/view/HalamanEdit.kt
+++ app/src/main/java/com/example/questapi_050/view/HalamanEdit.kt
@@ -1,2 +1,19 @@
 package com.example.questapi_050.view
 
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.questapi_050.viewmodel.EditViewModel
+import com.example.questapi_050.viewmodel.provider.PenyediaViewModel
+import java.lang.reflect.Modifier
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    navigateBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+
+}
