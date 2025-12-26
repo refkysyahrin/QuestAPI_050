a93db777d34b4ec933208fa5d7f2898778848898
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 14:53:48 +0700

    menambahkan initializer untuk DetailViewModel dan EditViewModel pada factory penyedia viewmodel

--- app/src/main/java/com/example/questapi_050/viewmodel/provider/PenyediaViewModel.kt
+++ app/src/main/java/com/example/questapi_050/viewmodel/provider/PenyediaViewModel.kt
@@ -1,18 +1,19 @@
 package com.example.questapi_050.viewmodel.provider
 
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.createSavedStateHandle
 import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.questapi_050.repositori.AplikasiDataSiswa
+import com.example.questapi_050.viewmodel.DetailViewModel
+import com.example.questapi_050.viewmodel.EditViewModel
 import com.example.questapi_050.viewmodel.EntryViewModel
 import com.example.questapi_050.viewmodel.HomeViewModel
 
 fun CreationExtras.aplikasiDataSiswa(): AplikasiDataSiswa = (
-        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as
-                AplikasiDataSiswa
+        this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as AplikasiDataSiswa
         )
-
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer {
@@ -20,6 +21,18 @@ object PenyediaViewModel {
         }
         initializer {
             EntryViewModel(aplikasiDataSiswa().container.repositoryDataSiswa)
+        }
+        initializer {
+            DetailViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
+        }
+        initializer {
+            EditViewModel(
+                this.createSavedStateHandle(),
+                aplikasiDataSiswa().container.repositoryDataSiswa
+            )
         }
     }
 }
