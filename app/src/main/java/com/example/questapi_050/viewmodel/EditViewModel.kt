8aa8cda6c22827c39d9a60abd50ed44689e1f298
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 14:21:05 +0700

    implementasi logika pengambilan data detail siswa berdasarkan id pada init block viewmodel

--- app/src/main/java/com/example/questapi_050/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_050/viewmodel/EditViewModel.kt
@@ -5,10 +5,23 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.questapi_050.modeldata.UIStateSiswa
+import com.example.questapi_050.modeldata.toUiStateSiswa
 import com.example.questapi_050.repositori.RepositoryDataSiswa
+import kotlinx.coroutines.launch
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
         private set
+
+    private val idSiswa: Int = checkNotNull(savedStateHandle[DestinasiDetail.itemIdArg])
+    init {
+        viewModelScope.launch {
+            uiStateSiswa = repositoryDataSiswa.getSatuSiswa(idSiswa)
+                .toUiStateSiswa(true)
+        }
+    }
+
+
     }
