b0706297947198967674622d3799158ebc4a1671
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 14:34:38 +0700

    membuat sealed interface StatusUIDetail untuk mengelola state loading, sukses, dan error pada halaman detail

--- app/src/main/java/com/example/questapi_050/viewmodel/DetailViewModel.kt
+++ app/src/main/java/com/example/questapi_050/viewmodel/DetailViewModel.kt
@@ -1,2 +1,11 @@
 package com.example.questapi_050.viewmodel
 
+import com.example.questapi_050.modeldata.DataSiswa
+
+sealed interface StatusUIDetail{
+    data class Success(val satusiswa: DataSiswa) : StatusUIDetail
+    object Error : StatusUIDetail
+    object Loading : StatusUIDetail
+}
+
+
