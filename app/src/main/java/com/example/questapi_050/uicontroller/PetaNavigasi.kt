617941bd37522ceb9e244f230f351290a7839d7e
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 16:55:20 +0700

    implementasi NavHost di PetaNavigasi.kt untuk mengatur navigasi antar screen (Home, Entry, Detail, Edit)

--- app/src/main/java/com/example/questapi_050/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_050/uicontroller/PetaNavigasi.kt
@@ -3,11 +3,17 @@ package com.example.questapi_050.uicontroller
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.questapi_050.uicontroller.route.DestinasiDetail
+import com.example.questapi_050.uicontroller.route.DestinasiEdit
 import com.example.questapi_050.uicontroller.route.DestinasiEntry
 import com.example.questapi_050.uicontroller.route.DestinasiHome
+import com.example.questapi_050.view.DetailSiswaScreen
+import com.example.questapi_050.view.EditSiswaScreen
 import com.example.questapi_050.view.EntrySiswaScreen
 import com.example.questapi_050.view.HomeScreen
 
@@ -29,6 +35,19 @@ fun HostNavigasi(
         }
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)})
+        }
+        composable(
+            DestinasiDetail.routeWithArgs, arguments = listOf(navArgument(DestinasiDetail
+            .itemIdArg) {
+            type = NavType.IntType })
+        ) {
+            DetailSiswaScreen(navigateToEditItem = {navController.navigate("${DestinasiEdit.route}/$it")},
+                navigateBack = {navController.navigate(DestinasiHome.route) })
+        }
+        composable(DestinasiEdit.routeWithArgs, arguments = listOf(navArgument(DestinasiEdit.itemIdArg) {
+            type = NavType.IntType })) {
+            EditSiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)},
+                onNavigateUp = {navController.navigateUp() })
         }
     }
 }
