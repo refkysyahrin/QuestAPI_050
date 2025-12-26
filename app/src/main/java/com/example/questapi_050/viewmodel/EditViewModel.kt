d1cc866d2e441b3286743575984ed08b013c5640
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 16:51:56 +0700

    implementasi EditViewModel untuk memuat data awal siswa dan logika update data ke repository

--- app/src/main/java/com/example/questapi_050/viewmodel/EditViewModel.kt
+++ app/src/main/java/com/example/questapi_050/viewmodel/EditViewModel.kt
@@ -8,9 +8,12 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.questapi_050.modeldata.DetailSiswa
 import com.example.questapi_050.modeldata.UIStateSiswa
+import com.example.questapi_050.modeldata.toDataSiswa
 import com.example.questapi_050.modeldata.toUiStateSiswa
 import com.example.questapi_050.repositori.RepositoryDataSiswa
+import com.example.questapi_050.uicontroller.route.DestinasiDetail
 import kotlinx.coroutines.launch
+import retrofit2.Response
 
 class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDataSiswa: RepositoryDataSiswa): ViewModel() {
     var uiStateSiswa by mutableStateOf(UIStateSiswa())
@@ -30,6 +33,18 @@ class EditViewModel(savedStateHandle: SavedStateHandle, private val repositoryDa
     private fun validasiInput(uiState: DetailSiswa = uiStateSiswa.detailSiswa): Boolean{
         return with(uiState){
             nama.isNotBlank() && alamat.isNotBlank() && telpon.isNotBlank()
+        }
+    }
+    suspend fun editSatuSiswa(){
+        if (validasiInput(uiStateSiswa.detailSiswa)) {
+            val call: Response<Void> = repositoryDataSiswa.editSatuSiswa(idSiswa,uiStateSiswa
+                .detailSiswa.toDataSiswa())
+
+            if (call.isSuccessful){
+                println("Update Sukses : ${call.message()}")
+            }else{
+                println("Update Error : ${call.errorBody()}")
+            }
         }
     }
     }
