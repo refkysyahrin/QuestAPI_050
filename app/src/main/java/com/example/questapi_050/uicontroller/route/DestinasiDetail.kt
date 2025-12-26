5962eebed80ebb6efc2b90972188fa9a759f5da0
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 16:29:21 +0700

    mendefinisikan objek DestinasiDetail lengkap dengan route dan argumen idSiswa untuk navigasi

--- app/src/main/java/com/example/questapi_050/uicontroller/route/DestinasiDetail.kt
+++ app/src/main/java/com/example/questapi_050/uicontroller/route/DestinasiDetail.kt
@@ -1,2 +1,10 @@
 package com.example.questapi_050.uicontroller.route
 
+import com.example.questapi_050.R
+
+object DestinasiDetail: DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
