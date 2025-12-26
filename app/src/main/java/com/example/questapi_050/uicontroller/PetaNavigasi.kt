454cdf0c313fa2a7f6c0915556ffc11d2979c9ec
refkysyahrin <refkymuhammadsyahrin@gmail.com>
2025-12-26 19:30:46 +0700

    Memperbaiki rute navigasi home ke detail siswa

--- app/src/main/java/com/example/questapi_050/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/questapi_050/uicontroller/PetaNavigasi.kt
@@ -27,11 +27,12 @@ fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
 ){
-    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier){
+    NavHost(navController = navController, startDestination = DestinasiHome.route, modifier = Modifier
+    ){
         composable(DestinasiHome.route){
-            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route)},
-                navigateToItemUpdate = { navController.navigate("${DestinasiEntry.route}/${it}")
-                })
+            HomeScreen(navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = {
+                    navController.navigate("${DestinasiDetail.route}/${it}") })
         }
         composable(DestinasiEntry.route){
             EntrySiswaScreen(navigateBack = {navController.navigate(DestinasiHome.route)})
